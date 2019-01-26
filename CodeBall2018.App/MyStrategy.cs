c7b964dd75803f73ceb6ff6dad6b178b4b23754e
lancer7082 <renhair@list.ru>
2019-01-26 23:24:31 +0300

    project

---
+++ CodeBall2018.App/MyStrategy.cs
@@ -0,0 +1,16 @@
+using Com.CodeGame.CodeBall2018.DevKit.CSharpCgdk.Model;
+
+namespace Com.CodeGame.CodeBall2018.DevKit.CSharpCgdk
+{
+    public sealed class MyStrategy : IStrategy
+    {
+        public void Act(Robot me, Rules rules, Game game, Action action)
+        {
+        }
+        
+        public string CustomRendering()
+        {
+            return "";
+        }
+    }
+}
